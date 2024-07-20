dbbd3ebd6dd0f6ebdcdaa96431db7c49792b5657
Saeed Altout <saeedaltoutpro@gmail.com>
2024-07-20 10:11:22 +0300

    fix and clean code

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
-// middleware.ts
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import NextAuth from "next-auth";
+
 import authConfig from "@/auth.config";
 import {
   DEFAULT_LOGIN_REDIRECT,
@@ -10,38 +8,42 @@ import {
   publicRoutes,
 } from "@/routes";
 
-export default withAuth(
-  function middleware(req: NextRequest) {
-    const { nextUrl } = req;
-    const isLoggedIn = !!req.nextauth.token;
+const { auth } = NextAuth(authConfig);
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-    if (isApiAuthRoute) {
-      return NextResponse.next();
-    }
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if (isAuthRoute) {
-      if (isLoggedIn) {
-        return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-      }
-      return NextResponse.next();
-    }
-
-    if (!isLoggedIn && !isPublicRoute) {
-      return NextResponse.redirect(new URL(`/auth/login`, nextUrl));
-    }
-
-    return NextResponse.next();
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
+  if (isApiAuthRoute) {
+    return null;
   }
-);
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(
+      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    );
+  }
+
+  return null;
+});
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
